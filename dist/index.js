cb6abf31466bf230bb64321617823a7c5acc4508
masete <nicholasmasete72@gmail.com>
2021-12-20 10:53:16 +0300

    some of ts types

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// Basic types
+let id = 5;
+let company = 'Manic media';
+let IsPublished = true;
+let x = 'hello';
+// arrays
+let ids = [1, 3, 4, 9];
+let arr = ['nick', 5, true];
+// tuple
+let person = [12, 'Nick', true];
+// array of tuples
+let employee;
+employee = [[12, 'james'],
+    [13, 'dan'],
+    [87, 'gerald']];
+// unions
+let pid;
+pid = 22;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+// Basic types
 let id: number = 5
+let company: string = 'Manic media'
+let IsPublished: Boolean = true
+let x: any = 'hello'
+
+// arrays
+let ids: number[] = [1,3,4,9]
+let arr: any[] = ['nick', 5, true]
+
+// tuple
+let person: [number, string, boolean] = [12, 'Nick', true]
+// array of tuples
+
+let employee: [number, string][] 
+
+
+ employee = [[12, 'james'], 
+[13, 'dan'], 
+[87, 'gerald']]
+
+// unions
+let pid: string | number
+
+pid = 22 
